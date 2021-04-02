07e4fdc489d5a27e6dec6bdca20a9962f4eee5be
Robin Newhouse <robin.newhouse@cern.ch>
2021-04-01 17:12:01 -0700

    updated setup script to use python3 and uproot3 (not 4)

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh
-source /cvmfs/sft.cern.ch/lcg/views/LCG_96c_LS/x86_64-centos7-gcc8-opt/setup.sh
-lsetup git
+source /cvmfs/sft.cern.ch/lcg/views/LCG_98python3/x86_64-centos7-gcc8-opt/setup.sh
+# lsetup git
 
 # Installing python directory
 # get directory of setup.sh script
