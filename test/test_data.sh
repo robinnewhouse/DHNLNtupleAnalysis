3698f9751fc2175973e7fd2472b4fa2254da74b4
Robin Newhouse <robin.newhouse@cern.ch>
2021-05-18 14:25:44 -0700

    Added more MC and data tests

---
+++ test/test_data.sh
@@ -0,0 +1,7 @@
+makeHistograms.py \
+--input root://eosuser.cern.ch//eos/home-d/dhnl/ntuple-ci/ntuples/v5p0_ntuples/data/group.phys-exotics.data18_ntuple_v5p0.v2_tree.root/group.phys-exotics.25411062._000215.tree.root \
+--config $WorkDir/data/SSbkg/config_SSbkg_uuu.json \
+--output_file $WorkDir/test/output/config_data_SSbkg_uuu.root \
+--analysis run2Analysis \
+--nevents 10000 \
+--force 
