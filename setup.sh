e9887a9c294bbb4153caf033206cf9bfd0d412e4
Robin Newhouse <robin.newhouse@cern.ch>
2021-05-18 19:50:35 +0000

    Update setup.sh, .gitlab-ci.yml files

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # lsetup git
 # Set up python from cvmfs
 # python 3
-source /cvmfs/sft.cern.ch/lcg/views/LCG_98python3/x86_64-centos7-gcc8-opt/setup.sh
+# source /cvmfs/sft.cern.ch/lcg/views/LCG_98python3/x86_64-centos7-gcc8-opt/setup.sh
 # # python 2
 # # source /cvmfs/sft.cern.ch/lcg/views/dev4python2/latest/x86_64-centos7-gcc8-opt/setup.sh
 
