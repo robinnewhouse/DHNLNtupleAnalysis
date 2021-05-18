dfac7095914cc40530b14c0243474789e7355bb1
Robin Newhouse <robin.newhouse@cern.ch>
2021-05-18 20:18:06 +0000

    Update test/test_mc_uuu.sh

--- test/test_mc_uuu.sh
+++ test/test_mc_uuu.sh
@@ -1,5 +1,5 @@
 makeHistograms.py \
---input root://eosuser.cern.ch//eos/home-d/dhnl/ntuple-ci/ntuples/v5p2_ntuples/mc/group.phys-exotics.mc16_13TeV.311651.DAOD_EXOT29.e7422_e5984_a875_r11891_r11748_p4482.dhnl_ntuple_v5p2.v1_tree.root/group.phys-exotics.25605658._000001.tree.root \
+--input /eos/home-d/dhnl/ntuple-ci/ntuples/v5p2_ntuples/mc/group.phys-exotics.mc16_13TeV.311651.DAOD_EXOT29.e7422_e5984_a875_r11891_r11748_p4482.dhnl_ntuple_v5p2.v1_tree.root/group.phys-exotics.25605658._000001.tree.root \
 --config $WorkDir/data/mc/config_mc_uuu.json \
 --output_file $WorkDir/test/output/config_mc_uuu.root \
 --analysis run2Analysis \
