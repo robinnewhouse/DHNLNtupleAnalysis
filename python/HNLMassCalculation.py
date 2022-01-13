bf3b85db0369b00f778248cc6dab14e496b92030
Dominique Anderson Trischuk <d.trischuk@cern.ch>
2022-01-12 16:09:15 -0800

    change 4-vector to 3-vector for simplicity

--- python/HNLMassCalculation.py
+++ python/HNLMassCalculation.py
@@ -6,11 +6,12 @@ class HNLMassCalculation:
     """
     The class used to compute the HNL mass (m_HNL) for each event.
     Inputs:
-    - pv: ROOT TVector3  object for the location (x,y,z) of the primary vertx (PV). See: https://root.cern.ch/doc/master/classTVector3.html
-    - dv: ROOT TVector3 object for the location (x,y,z) of the displaced vertx (DV). See: https://root.cern.ch/doc/master/classTVector3.html
-    - displaced_leptons: Python list of ROOT TLorentzVector objects (px, py, pz, E) for two tracks in the DV. See: https://root.cern.ch/doc/master/classTLorentzVector.html
-    - prompt_lepton: ROOT TLorentzVector object (px, py, pz, E) for prompt lepton. See: https://root.cern.ch/doc/master/classTLorentzVector.html
-    - fix_w_mass: A boolean flag that fixes the W mass to the pole mass in the energy momentum conservation expression. Default: False
+    - pv: ROOT TVector3 object for the location (x,y,z) of the primary vertx (PV).
+    - dv: ROOT TVector3 object for the location (x,y,z) of the displaced vertx (DV).
+    - displaced_leptons: Python list of ROOT TVector3 objects for the momenta (px,py, pz) of the two leptons in the DV. Units: GeV.
+    - prompt_lepton: ROOT TVector3 object for the momentum (px, py, pz) of the prompt lepton. Units: GeV.
+    - fix_w_mass: A boolean flag that fixes the W mass to the pole mass in the energy momentum conservation expression. Default: False.
+    For details about ROOT TVector3 objects see: https://root.cern.ch/doc/master/classTVector3.html
     """
 
     def __init__(self, pv, dv, displaced_leptons, prompt_lepton, fix_w_mass=False):
@@ -26,11 +27,11 @@ class HNLMassCalculation:
         self.no_solution = -1
 
         # Assign p0 to be the 3-vector of the prompt lepton
-        p0 = ROOT.TVector3(prompt_lepton.Px(), prompt_lepton.Py(), prompt_lepton.Pz())
+        p0 = ROOT.TVector3(prompt_lepton.X(), prompt_lepton.Y(), prompt_lepton.Z())
 
         # Assign d0 and d1 to be the 3-vectors for the displaced leptons
-        d0 = ROOT.TVector3(displaced_leptons[0].Px(), displaced_leptons[0].Py(), displaced_leptons[0].Pz())
-        d1 = ROOT.TVector3(displaced_leptons[1].Px(), displaced_leptons[1].Py(), displaced_leptons[1].Pz())
+        d0 = ROOT.TVector3(displaced_leptons[0].X(), displaced_leptons[0].Y(), displaced_leptons[0].Z())
+        d1 = ROOT.TVector3(displaced_leptons[1].X(), displaced_leptons[1].Y(), displaced_leptons[1].Z())
 
         # Choose z direction to be along decay
         decayV = dv - pv
@@ -146,15 +147,15 @@ if __name__ == "__main__":
     print("HNL mass calculation examples", "\n")
 
     # example 1
-    print("Example 1 is from an MC generated 10 GeV HNL")
+    print("Example 1 is from a simulated event with a 10 GeV HNL")
     dv_1 = ROOT.TVector3(15.512019157409668, 19.17801856994629, -107.96952056884766)
     pv_1 = ROOT.TVector3(-0.5000697374343872, -0.5047531723976135, -40.7725830078125)
-    displaced_leptons_1 = [ROOT.TLorentzVector(), ROOT.TLorentzVector()]
-    displaced_leptons_1[0].SetPxPyPzE(2.48017372631826, -0.2136493157850394, -4.773501905451388, 666)
-    displaced_leptons_1[1].SetPxPyPzE(9.90227566884341, 15.053740580375933, -43.85763404407506, 666)
-    prompt_lepton_1 = ROOT.TLorentzVector()
-    prompt_lepton_1.SetPxPyPzE(-19.65923058291866, -32.908382397006065, -198.16919648843634, 666)
+    displaced_leptons_1 = [ROOT.TVector3(), ROOT.TVector3()]
+    displaced_leptons_1[0].SetXYZ(2.48017372631826, -0.2136493157850394, -4.773501905451388)
+    displaced_leptons_1[1].SetXYZ(9.90227566884341, 15.053740580375933, -43.85763404407506)
+    prompt_lepton_1 = ROOT.TVector3()
+    prompt_lepton_1.SetXYZ(-19.65923058291866, -32.908382397006065, -198.16919648843634)
     example_1 = HNLMassCalculation(pv_1, dv_1, displaced_leptons_1, prompt_lepton_1, fix_w_mass=False)
 
-    print("Truth HNL mass:", "10 GeV")
+    print("Simluated HNL mass:", "10 GeV")
     print("Calculated HNL mass:", example_1.mhnl, "GeV")
